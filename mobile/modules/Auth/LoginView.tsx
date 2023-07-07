f5da70c4638ad21f438bf7ca9d53ed2d8ed0a8ab
dhairyam2002 <dhairyam2002@gmail.com>
2023-07-07 19:22:15 +0530

    deep-linking

--- mobile/modules/Auth/LoginView.tsx
+++ mobile/modules/Auth/LoginView.tsx
@@ -1,5 +1,5 @@
 import { Image, Pressable, Text, ToastAndroid, TouchableOpacity, View } from "react-native"
-import React from "react"
+import React, { useDeferredValue, useEffect, useState } from "react"
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamsList } from "../../Navigation";
 import { useNavigation } from "@react-navigation/native";
@@ -7,22 +7,41 @@ import { MainText } from "../../ui/Text";
 import { AntDesign } from '@expo/vector-icons'; 
 import * as WebBrowser from 'expo-web-browser';
 import { GOOGLE_CLIENT_ID, GOOGLE_REDIRECT_URI } from "../../envConstants";
+import * as Linking from "expo-linking";
 
 type Props = NativeStackNavigationProp<RootStackParamsList, 'Login'>;
 export const LoginView = function () {
     const navigation = useNavigation<Props>();
     const clientId = GOOGLE_CLIENT_ID
     const redirectUri = GOOGLE_REDIRECT_URI
+
+    
+    const [result, setResult] = useState<WebBrowser.WebBrowserAuthSessionResult>();
     async function handleGoogleSignIn() {
         console.log(clientId);
         console.log(redirectUri)
-        const result = await WebBrowser.openAuthSessionAsync(`https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile&access_type=offline&state=1234_purpleGoogle&prompt=consent`, redirectUri);
+        const url =  Linking.createURL("app")
+        console.log(url);
 
+        const result = await WebBrowser.openAuthSessionAsync(`https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile&access_type=offline&state=1234_purpleGoogle&prompt=consent`, url);
 
-        if (result.type === 'success') {
-            
-        }
+        setResult(result);
+        console.log('got the result')
+    //     if (result.type === 'success') {
+    //         const params = Linking.parse(result.url);
+    //         console.log(params.queryParams);
+    //     }
     }
+
+
+    useEffect(() => {
+        if(result && result.type === 'success') {
+            const params = Linking.parse(result.url);
+            console.log(params);
+        }
+        console.log('result changed');
+        console.log(result)
+    }, [result])
     return (
         <View style={{flex: 1, backgroundColor: 'black', justifyContent: 'center', alignItems: 'center'}}>
             <Image source={require('../../assets/logo-transparent-png.png')} style={{width: 200, height: 200}}></Image>
@@ -39,7 +58,13 @@ export const LoginView = function () {
                 }}>Continue With Google</Text>
             </Pressable>
 
-            <Pressable style={{borderColor: 'grey', borderWidth: 1, padding: 15, borderRadius: 30, flexDirection: 'row', alignItems: 'center', marginTop: 20}}>
+            <Pressable style={{borderColor: 'grey', borderWidth: 1, padding: 15, borderRadius: 30, flexDirection: 'row', alignItems: 'center', marginTop: 20}} onPress={() => {
+                fetch('https://streamsonic.loca.lt/').then((res) => res.json()).then((data) => {
+                    console.log(data)
+                }).catch((err) => {
+                    console.log(err)
+                })
+            }}>
             <AntDesign name="twitter" size={22} color={'white'} />
                 <Text style={{
                     color: 'white',
